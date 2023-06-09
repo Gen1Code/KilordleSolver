67efbc3038df10e8c39b0ef24adf33a16db7c3b8
Gen1Code <72555054+Gen1Code@users.noreply.github.com>
2023-06-09 02:27:00 +0100

    fixed Java ver
    Added TODO notes/ideas
    simplified code

--- JavaVer/src/Main.java
+++ JavaVer/src/Main.java
@@ -1,5 +1,6 @@
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.*;
 import static java.util.Map.entry;
 
@@ -55,14 +56,17 @@ public class Main {
     );
 
 
-    public static void generateAlphabetLibrary(ArrayList<String> words){
+    public static void generateAlphabetLibrary(ArrayList<String> words) throws IOException{
         System.out.println("Generating Alpha Library");
         //Generate needed characters in each positions from wordle list
-        //with open("..\Wordles.txt") as f:
-        //for line in f:
-        //words.append(line.removesuffix("\n"))
+        BufferedReader br = new BufferedReader(new FileReader("..\\Wordles.txt"));
+        String line;
+        while((line= br.readLine()) != null){
+            words.add(line);
+        }
+        br.close();
 
-
+        alphabet = new String[]{"","","","",""};
         for(String word : words){
             for(int i=0;i<5;i++){
                 if(alphabet[i].indexOf(word.charAt(i)) == -1){
@@ -77,13 +81,13 @@ public class Main {
             Arrays.sort(tmp);
             alphabet[i] = new String(tmp);
         }
-        System.out.println(alphabet);
+        System.out.println(Arrays.toString(alphabet));
     }
 
     public static boolean containall(String[] givenarray,int size){
         boolean flag = true;
         for(int i=0; i<5;i++){
-            String tempalpha = alphabet[i];
+            String tempalpha = alphabet[i]; //mutability??
             for(int j=0; j<size;j++){
                 tempalpha = tempalpha.replace(String.valueOf(givenarray[j].charAt(i)),"");
             }
@@ -129,35 +133,35 @@ public class Main {
         return count;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         System.out.println("Running");
+
         double lastmax;
         double max = 0;
-        ArrayList<String> allwords;
-
         ArrayList<String> words  = new ArrayList<>();
 
+        //Adds Wordles.txt to words
         generateAlphabetLibrary(words);
 
-        try {
-            Scanner s = new Scanner(new File("../Words.txt"));
-            allwords = new ArrayList<String>(Arrays.asList(s.nextLine().replace("\"","").split(",")));
-            s.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace(System.err);
-            return;
+        //Adds Words.txt to words
+        BufferedReader br = new BufferedReader(new FileReader("..\\Words.txt"));
+        String line;
+        while((line= br.readLine()) != null){
+            words.add(line);
         }
+        br.close();
+
 
         int min = 999;
         String[] minarr = new String[]{};
 
         //the bigger the range the better the result, set at 50 for a good heuristic, >200000 is stupid
-        for (int k=0;k<30000;k++){
+        for (int k=0;k<100;k++){
             String[] found = new String[40];
             int size = 0;
 
             //shuffle the words to change which word is picked when information gain is the same
-            Collections.shuffle(allwords);
+            Collections.shuffle(words);
 
             //while not enough information found keep adding words
             while(true) {
@@ -168,7 +172,7 @@ public class Main {
                 boolean beg = size <= 15; //15 has given me the lowest guesses:31
 
                 //Find the amount of information gained for each word, add the word that gives most information
-                for (String word : allwords) {
+                for (String word : words) {
                     double temp = newAlphaLocations(AlphaLibrary, word, beg);
                     if (temp > max) {
                         max = temp;
@@ -194,6 +198,11 @@ public class Main {
         }
         System.out.println(Arrays.toString(minarr));
         System.out.println(min);
+
+        //TODO
+        //add score for keeping largest amount of words available if a tie in information /5
+        //printing stuff
+        //speed
     }
 
 
